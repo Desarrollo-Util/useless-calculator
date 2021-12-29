5ea3d06aaee55c84114e3df26110549b43566727
Pablo Castellanos <pablocd168@gmail.com>
2021-12-29 19:59:41 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Useless-calculator
+
+Una librería que suma, resta y multiplica, y por lo demás es completamente inútil.
---
+++ src/index.js
@@ -0,0 +1,21 @@
+/**
+ * Adds two numbers
+ * @param a Number one
+ * @param b Number two
+ * @returns Result
+ */
+export var add = function (a, b) { return a + b; };
+/**
+ * Substracts two numbers
+ * @param a Number one
+ * @param b Number two
+ * @returns Result
+ */
+export var subtract = function (a, b) { return a - b; };
+/**
+ * Multiplies two numbers
+ * @param a Number one
+ * @param b Number two
+ * @returns Result
+ */
+export var multiply = function (a, b) { return a * b; };
